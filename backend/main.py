8f75aa94ce1ea2122fe5a0bfa80546f5bdc1d671
Ovais <ovais@example.com>
2025-12-27 23:37:25 +0500

    Deploy backend

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # In production, specify exact origins
+    allow_origins=[" https://mohammad-ovais.github.io/hackathon_project1/"],  # In production, specify exact origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
